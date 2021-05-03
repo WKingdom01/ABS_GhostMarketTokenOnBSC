91b81ecf886923eaf1c3ad0cfdb213b0c494e078
WKindom01 <minatotanaka512@gmail.com>
2021-05-03 19:35:51 +0200

    renamed NFT contract to ERC771

--- test/include_in_tesfiles.js
+++ test/include_in_tesfiles.js
@@ -1,4 +1,4 @@
-const GhostmarketNFT = artifacts.require("GhostmarketNFT");
+const GhostmarketERC721 = artifacts.require("GhostmarketERC721");
 const TOKEN_NAME = "Ghostmarket NFT"
 const TOKEN_SYMBOL = "GMNFT"
 const TOKEN_URI = "my.app/"
@@ -6,7 +6,7 @@ const MINTER_ROLE = "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981
 const PAUSER_ROLE = "0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a"
 
 exports._t_c = {
-  GhostmarketNFT: GhostmarketNFT,
+  GhostmarketERC721: GhostmarketERC721,
   TOKEN_NAME: TOKEN_NAME,
   TOKEN_SYMBOL: TOKEN_SYMBOL,
   TOKEN_URI: TOKEN_URI
