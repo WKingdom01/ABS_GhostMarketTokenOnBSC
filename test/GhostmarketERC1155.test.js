23ffcd8530b40f4ee225421a87a0ab057b263ffb
WKindom01 <minatotanaka512@gmail.com>
2021-05-09 23:42:29 +0300

    ERC1155 contract
    upgradable, pausable, burnable
    added sale royalities and minting fee
    
    issue #18
    close #13 #12 #11 #10

--- test/GhostmarketERC1155.test.js
+++ test/GhostmarketERC1155.test.js
@@ -14,7 +14,8 @@ const { ZERO_ADDRESS } = constants;
 var my_constants = require('./include_in_tesfiles.js')
 
 // Start test block
-contract('GhostmarketERC721', async accounts => {
+contract('GhostmarketERC1155', async accounts => {
+  const [minter, transferToAccount, royalitiesAccount, mintingFeeAccount, royalitiesAccount2] = accounts;
   before(async function () {
     // Deploy a new contract before the tests
     this.GhostmarketERC1155 = await deployProxy(
@@ -43,168 +44,188 @@ contract('GhostmarketERC721', async accounts => {
 
 
 
-  it('deployer can mint tokens with royalitiy fees and attributes', async function () {
+  it('deployer can mint tokens with royalty fee', async function () {
     const tokenId = new BN('0');
-    const minter = accounts[0];
-    const to = accounts[1];
-
     const mintAmount = new BN(2);
-    const burnAmount = new BN(3000);
-
-
-
     const data = '0x987654321';
+    const value = 88
+    const counter = parseInt((await this.GhostmarketERC1155.getCurrentCounter()).toString())
 
-    const receipt = await this.GhostmarketERC1155.mint(to, mintAmount, data);
-    expectEvent(receipt, 'TransferSingle', { operator: minter, from: ZERO_ADDRESS, to: to, id: tokenId, value: mintAmount });
-    expect((await this.GhostmarketERC1155.getCurrentCounter())).to.be.bignumber.equal('1');
+    const receipt = await this.GhostmarketERC1155.mint(transferToAccount, mintAmount, data, [{ recipient: minter, value: value }]);
+    expectEvent(receipt, 'TransferSingle', { operator: minter, from: ZERO_ADDRESS, to: transferToAccount, id: tokenId, value: mintAmount });
+    expect(parseInt(((await this.GhostmarketERC1155.getCurrentCounter()).toString()))).to.equal(counter + 1);
 
-
-
-    /*     
-        expect(await this.GhostmarketERC1155.balanceOf(accounts[1])).to.be.bignumber.equal('1');
-        expect(await this.GhostmarketERC1155.ownerOf(tokenId)).to.equal(accounts[1]);
-    
-        expect(await this.GhostmarketERC1155.tokenURI(tokenId)).to.equal(my_constants._t_c.TOKEN_URI + tokenId);
-    
-        const values = await this.GhostmarketERC1155.getFeeBps(tokenId);
-        expect(values.length).to.equal(1);
-        expect(values[0]).to.be.bignumber.equal('100');
-    
-        const attributes = await this.GhostmarketERC1155.getAttributes(tokenId);
-        expect(attributes[0].key).to.equal("bar");
-        expect(attributes[0].value).to.equal("1234");
-        expect(attributes[1].key).to.equal("baz");
-        expect(attributes[1].value).to.equal("strong");
-        */
-
+    const values = await this.GhostmarketERC1155.getRoyaltyFeeBps(tokenId);
+    expect(values.length).to.equal(1);
+    expect(values[0]).to.be.bignumber.equal(value.toString());
   });
-  it.only('deployer can mint tokens as batch with royalitiy fees and attributes', async function () {
-    const operator = accounts[0]
-    const tokenBatchHolder = accounts[2];
-    const tokenBatchIds = [new BN(2000), new BN(2010), new BN(2020)];
+
+  it('should mint tokens as batch with royalty fee', async function () {
     const mintAmounts = [new BN(5000), new BN(10000), new BN(42195)];
-    const burnAmounts = [new BN(5000), new BN(9001), new BN(195)];
+    const royalities = [{ recipient: royalitiesAccount, value: 50 }, { recipient: royalitiesAccount2, value: 100 }, { recipient: royalitiesAccount2, value: 30 }]
     const data = '0x987654321';
+    const currentCounter = parseInt(await this.GhostmarketERC1155.getCurrentCounter())
+    console.log("currentCounter: ", currentCounter)
+    console.log("royalities array length: ", royalities.length)
 
     const receipt = await this.GhostmarketERC1155.mintBatch(
-      tokenBatchHolder,
+      transferToAccount,
       mintAmounts,
       data,
-      { from: operator },
+      royalities,
+      { from: minter }
     )
-    expectEvent(receipt, 'TransferBatch', { operator: operator, from: ZERO_ADDRESS, to: tokenBatchHolder });
-
-
-  });
-  it('deployer can mint tokens WITHOUT royalitiy fees and attributes', async function () {
-    const minter_account = accounts[5];
-
-    const receipt = await this.GhostmarketERC1155.mint(minter_account, [], [], "");
-
-    const tokenId = new BN(parseInt(await this.GhostmarketERC1155.getCurrentCounter()) - 1)
-    console.log("tokenId: ", tokenId)
-
-    expectEvent(receipt, 'Transfer', { from: ZERO_ADDRESS, to: minter_account, tokenId });
-
-    expect(await this.GhostmarketERC1155.balanceOf(minter_account)).to.be.bignumber.equal('1');
-    expect(await this.GhostmarketERC1155.ownerOf(tokenId)).to.equal(minter_account);
-
-    expect(await this.GhostmarketERC1155.tokenURI(tokenId)).to.equal(my_constants._t_c.TOKEN_URI + tokenId);
-
-    const values = await this.GhostmarketERC1155.getFeeBps(tokenId);
-    console.log("royalitiy fee values: ", values)
-    expect(values).to.be.empty;
-
-    const attributes = await this.GhostmarketERC1155.getAttributes(tokenId);
-    console.log("attributes : ", attributes)
-    expect(attributes).to.be.empty;
-
-
+    expectEvent(receipt, 'TransferBatch', { operator: minter, from: ZERO_ADDRESS, to: transferToAccount });
+    const calclastTokenID = currentCounter + royalities.length - 1
+    console.log("calclastTokenID: ", calclastTokenID)
+    console.log("getLastTokenID: ", await this.GhostmarketERC1155.getLastTokenID())
+    expect(await this.GhostmarketERC1155.getLastTokenID()).to.be.bignumber.equal((calclastTokenID).toString());
+    for (i = currentCounter; i <= calclastTokenID; i++) {
+      console.log("token ID: ", i)
+      if (royalities[i]) {
+        expect(parseInt(await this.GhostmarketERC1155.getRoyaltyFeeBps(i))).to.equal(royalities[i - currentCounter].value)
+      }
+    }
   });
 
-  it('non minter cannot mint', async function () {
-    // Test a transaction reverts
+  it('should revert with error: Fees array lenght should be less than or equal to mintAmounts array lenght', async function () {
+    const mintAmounts = [new BN(5000), new BN(10000), new BN(42195)];
+    const royalities = [{ recipient: royalitiesAccount, value: 50 }, { recipient: royalitiesAccount2, value: 100 }, { recipient: royalitiesAccount2, value: 30 }, { recipient: royalitiesAccount2, value: 1100 }]
+    const data = '0x987654321';
+    const currentCounter = parseInt(await this.GhostmarketERC1155.getCurrentCounter())
+    console.log("currentCounter: ", currentCounter)
+    console.log("royalities array length: ", royalities.length)
+
     await expectRevert(
-      this.GhostmarketERC1155.mint(accounts[2], [{ recipient: accounts[2], value: 100 }], [{ key: "bar", value: "1234" }], "", { from: accounts[3] }),
-      'ERC721PresetMinterPauserAutoId: must have minter role to mint'
+      this.GhostmarketERC1155.mintBatch(
+        transferToAccount,
+        mintAmounts,
+        data,
+        royalities,
+        { from: minter }
+      ),
+      'Fees array lenght should be less than or equal to mintAmounts array lenght'
     );
   });
 
-  it("allows to update royalities/secondary-sales fee recipient", async function () {
-    const tokenId = new BN('0')
-    const minter = accounts[1]
-    const feeRecepient = accounts[2]
-    console.log("minter account: ", minter)
-    console.log("new recipients account: ", feeRecepient)
-    const receipt = await this.GhostmarketERC1155.mint(minter, [{ recipient: minter, value: 100 }], [{ key: "bar", value: "1234" }], "")
 
-    await this.GhostmarketERC1155.updateFeeAccount(tokenId, minter, feeRecepient)
 
-    const recipients = await this.GhostmarketERC1155.getFeeRecipients(tokenId)
-    expect(recipients.length).to.equal(1)
-    expect(recipients[0]).to.equal(feeRecepient)
+  it('deployer can mint tokens WITHOUT royalitiy fees and attributes', async function () {
+    const mintAmounts = [new BN(5000), new BN(10000), new BN(42195)];
+    const royalities = []
+    const data = '0x987654321';
+    const currentCounter = parseInt(await this.GhostmarketERC1155.getCurrentCounter())
+    console.log("currentCounter: ", currentCounter)
+    console.log("royalities array length: ", royalities.length)
 
+    const receipt = await this.GhostmarketERC1155.mintBatch(
+      transferToAccount,
+      mintAmounts,
+      data,
+      royalities,
+      { from: minter }
+    )
+    expectEvent(receipt, 'TransferBatch', { operator: minter, from: ZERO_ADDRESS, to: transferToAccount });
+    const calclastTokenID = currentCounter + mintAmounts.length - 1
+    console.log("calclastTokenID: ", calclastTokenID)
+    console.log("getLastTokenID: ", await this.GhostmarketERC1155.getLastTokenID())
+    expect(await this.GhostmarketERC1155.getLastTokenID()).to.be.bignumber.equal((calclastTokenID).toString());
   });
 
-  it("allows to update royalities/secondary-sales fee value for specific account", async function () {
-    const newFeeValue = 66
-    const receipt = await this.GhostmarketERC1155.mint(accounts[1], [{ recipient: accounts[1], value: 100 }], [{ key: "bar", value: "1234" }], "");
-    const tokenId = new BN(parseInt(await this.GhostmarketERC1155.getCurrentCounter()) - 1)
+  it('should throw ERC1155PresetMinterPauser: must have minter role to mint', async function () {
+    const mintAmount = new BN(2);
+    const data = '0x987654321';
+    await expectRevert(
+      this.GhostmarketERC1155.mint(transferToAccount, mintAmount, data, [], { from: royalitiesAccount2 }),
+      'ERC1155PresetMinterPauser: must have minter role to mint'
+    );
+  });
+  describe('royalities', function () {
+    const mintAmount = new BN(2);
+    const data = '0x987654321';
+    const value = 88
 
-    await this.GhostmarketERC1155.updateRecipientsFees(tokenId, accounts[1], newFeeValue);
+    it("should update royalities/secondary-sales fee recipient", async function () {
 
-    const values = await this.GhostmarketERC1155.getFeeBps(tokenId);
-    expect(values.length).to.equal(1);
-    console.log("new fee value: ", values[0])
-    expect(values[0]).to.be.bignumber.equal((newFeeValue).toString());
+      console.log("old recipients account: ", royalitiesAccount)
+      console.log("new recipients account: ", royalitiesAccount2)
 
+      await this.GhostmarketERC1155.mint(minter, mintAmount, data, [{ recipient: royalitiesAccount, value: value }]);
+      const tokenId = (await this.GhostmarketERC1155.getLastTokenID()).toString()
+      console.log("getLastTokenID: ", tokenId)
+
+      const recipients = await this.GhostmarketERC1155.getRoyalitiesRecipients(tokenId)
+      expect(recipients[0]).to.equal(royalitiesAccount)
+
+      const receipt = await this.GhostmarketERC1155.updateRoyalitiesAccount(tokenId, royalitiesAccount, royalitiesAccount2)
+      expectEvent(receipt, 'RoyalitiesAccountChanged', { tokenId: tokenId, from: royalitiesAccount, to: royalitiesAccount2 });
+
+      const recipients2 = await this.GhostmarketERC1155.getRoyalitiesRecipients(tokenId)
+      expect(recipients2.length).to.equal(1)
+      expect(recipients2[0]).to.equal(royalitiesAccount2)
+    });
+
+    it("should update royalities/secondary-sales fee value", async function () {
+      const newFeeValue = new BN(66)
+      await this.GhostmarketERC1155.mint(minter, mintAmount, data, [{ recipient: royalitiesAccount, value: value }]);
+      const tokenId = (await this.GhostmarketERC1155.getLastTokenID()).toString()
+      console.log("getLastTokenID: ", tokenId)
+
+      const receipt = await this.GhostmarketERC1155.updateRoyalitiesFeeValue(tokenId, royalitiesAccount, newFeeValue);
+      expectEvent(receipt, 'RoyalitiesFeeValueChanged', { tokenId: tokenId, value: newFeeValue });
+
+      const values = await this.GhostmarketERC1155.getRoyaltyFeeBps(tokenId);
+      expect(values.length).to.equal(1);
+      console.log("new fee value: ", values[0])
+      expect(values[0]).to.be.bignumber.equal((newFeeValue).toString());
+    });
   });
 
-  describe('should mint NFT with fee', function () {
-    it('reverts on zero-valued minting fees', async function () {
-      const minter = accounts[1]
-      const feeAddress = accounts[3]
+  describe('mint NFT with fee', function () {
+    const mintAmount = new BN(2);
+    const data = '0x987654321';
+    it('should revert if minting fee is zero', async function () {
+
       const value = ether('0');
-      await this.GhostmarketERC1155.setGhostmarketFeeAddress(feeAddress)
+      await this.GhostmarketERC1155.setGhostmarketFeeAddress(mintingFeeAccount)
       await this.GhostmarketERC1155.setGhostmarketFeeMultiplier(1)
       await this.GhostmarketERC1155.setGhostmarketMintFee(value)
       await expectRevert(
-        this.GhostmarketERC1155.mintWithFee(minter, [], [], "", { value: value }),
+        this.GhostmarketERC1155.mint(minter, mintAmount, data, [], { value: value }),
         'Ghostmarket minting Fee is zero'
       );
     });
 
-    it('should requires a non-null minting fee address', async function () {
-      const minter = accounts[1]
-      const feeAddress = accounts[3]
+    it('should requires a minting fee address that is not 0x0', async function () {
+
       const value = ether('0.1');
       await this.GhostmarketERC1155.setGhostmarketFeeAddress(ZERO_ADDRESS)
       await this.GhostmarketERC1155.setGhostmarketFeeMultiplier(1)
       await this.GhostmarketERC1155.setGhostmarketMintFee(value)
       await expectRevert(
-        this.GhostmarketERC1155.mintWithFee(minter, [], [], "", { value: value }),
+        this.GhostmarketERC1155.mint(minter, mintAmount, data, [], { value: value }),
         'Ghostmarket minting Fee Address not set'
       );
     });
 
     it('should accept payments', async function () {
-      const minter = accounts[1]
-      const feeAddress = accounts[3]
+      const mintAmount = new BN(20);
+      const royalities = [{ recipient: royalitiesAccount, value: 50 }]
+      const ghostmarketFeeMultiplier = 1
       const value = ether('0.1');
-      await this.GhostmarketERC1155.setGhostmarketFeeAddress(feeAddress)
-      await this.GhostmarketERC1155.setGhostmarketFeeMultiplier(1)
+      await this.GhostmarketERC1155.setGhostmarketFeeAddress(mintingFeeAccount)
+      await this.GhostmarketERC1155.setGhostmarketFeeMultiplier(ghostmarketFeeMultiplier)
       await this.GhostmarketERC1155.setGhostmarketMintFee(value)
-      let feeAddressEthBalanceBefore = await web3.eth.getBalance(feeAddress)
+      let feeAddressEthBalanceBefore = await web3.eth.getBalance(mintingFeeAccount)
 
       console.log("ghostmarketFeeMultiplier: ", await this.GhostmarketERC1155.ghostmarketFeeMultiplier())
       console.log("ghostmarketMintingFee: ", await this.GhostmarketERC1155.ghostmarketMintingFee())
       console.log("ghostmarketFeeAddress: ", await this.GhostmarketERC1155.ghostmarketFeeAddress())
-      await this.GhostmarketERC1155.mintWithFee(minter, [], [], "", { value: value })
-      let feeAddressEthBalanceAfter = await web3.eth.getBalance(feeAddress)
+      await this.GhostmarketERC1155.mint(minter, mintAmount, data, royalities, { value: value });
+      let feeAddressEthBalanceAfter = await web3.eth.getBalance(mintingFeeAccount)
       console.log("feeAddress eth balance before: ", feeAddressEthBalanceBefore)
       console.log("feeAddress eth balance after: ", feeAddressEthBalanceAfter)
-      expect(parseInt(feeAddressEthBalanceAfter)).to.equal(parseInt(feeAddressEthBalanceBefore) + parseInt(value))
+      expect(parseInt(feeAddressEthBalanceAfter)).to.equal(parseInt(feeAddressEthBalanceBefore) + (parseInt(value) / (ghostmarketFeeMultiplier * parseInt(mintAmount))))
     });
   });
 
@@ -217,10 +238,6 @@ contract('GhostmarketERC721', async accounts => {
 
     console.log("token owner address: ", await this.GhostmarketERC1155.ownerOf(tokenId))
     const { logs } = await this.GhostmarketERC1155.getLockedContent.sendTransaction(tokenId, { from: nftReceiver })
-
-    //const { logs } = await this.GhostmarketERC1155.getLockedContent(tokenId)
-
-    //console.log("result: ", result)
 
     expectEvent.inLogs(logs, 'LockedContentViewed', {
       msgSender: nftReceiver,
